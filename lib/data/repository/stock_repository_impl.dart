1979e0a948567a99eb699934c960830d3773547e
choi-jae-woong <jason967@naver.com>
2022-09-06 17:53:25 +0900

    csv parser

---
+++ lib/data/repository/stock_repository_impl.dart
@@ -0,0 +1,52 @@
+import 'package:stock_app/data/csv/company_listings_parser.dart';
+import 'package:stock_app/data/mapper/company_mapper.dart';
+import 'package:stock_app/domain/model/company_listing.dart';
+import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/util/result.dart';
+
+import '../source/local/stock_dao.dart';
+import '../source/remote/stock_api.dart';
+
+class StockRepositoryImpl implements StockRepository {
+  final StockApi _api;
+  final StockDao _dao;
+  final _parser = CompanyListingsParser();
+
+  StockRepositoryImpl(this._api, this._dao);
+
+  @override
+  Future<Result<List<CompanyListing>>> getCompanyListings(
+      bool fetchFromRemote, String query) async {
+    //캐시에서 찾는다
+
+    final localListings = await _dao.searchCompanyListing(query);
+    //없다면 리모트에서 가져온다
+    final isDbEmpty = localListings.isEmpty && query.isEmpty;
+    final shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote;
+
+    //캐시
+    if (shouldJustLoadFromCache) {
+      return Result.success(
+          localListings.map((e) => e.toCompanyListing()).toList());
+    }
+
+    //리모트
+    try {
+      final reponse = await _api.getListings();
+
+      final remoteListings = await _parser.parse(reponse.body);
+
+      //캐시를 비워주고
+      await _dao.clearCompanyListings();
+
+      //캐시에 추가
+      await _dao.insertCompanyListings(
+        remoteListings.map((e) => e.toCompanyListingEntity()).toList(),
+      );
+
+      return Result.success(remoteListings);
+    } catch (e) {
+      return Result.error(Exception('데이터 로드 실패'));
+    }
+  }
+}
--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -2,13 +2,13 @@ import 'package:http/http.dart' as http;
 
 class StockApi {
   static const baseUrl = 'https://www.alphavantage.co/';
-  static const apiKey = '54L72Q8UIQVTB4O5';
-  final http.Client client;
+  static const _apiKey = '54L72Q8UIQVTB4O5';
+  final http.Client _client;
   //test코드를 작성할 때 용이
-  StockApi(this.client);
+  StockApi({http.Client? client}) : _client = (client ?? http.Client());
 
-  Future<http.Response> getListings(String apiKey) async {
-    return await client.get(Uri.parse(
-        'https://www.alphavantage.co/query?function=LISTING_STATUS&paikey=$apiKey'));
+  Future<http.Response> getListings({String apiKey = _apiKey}) async {
+    return await _client.get(Uri.parse(
+        'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apiKey'));
   }
 }
